22ff5bc9c51ee4e5a424eaba13d28486a5980d25
Lukas Žiūkas <lukasziukas18@gmail.com>
2020-09-23 16:18:36 +0300

    A

---
+++ Core/NoOpRunner.Networking/HostBridge.cs
@@ -0,0 +1,27 @@
+﻿using NoOpRunner.Core.Dtos;
+using System;
+
+namespace NoOpRunner.Networking
+{
+    public class HostBridge
+    {
+        private HostBridge()
+        {
+
+        }
+
+        public static HostBridge Bridge { get; } = new HostBridge();
+
+        private Action<MessageDto> MessageHandler;
+
+        public void RegisterHostMessageHandler(Action<MessageDto> handler)
+        {
+            MessageHandler = handler;
+        }
+
+        public void HandleHostMessage(MessageDto message)
+        {
+            MessageHandler?.Invoke(message);
+        }
+    }
+}
