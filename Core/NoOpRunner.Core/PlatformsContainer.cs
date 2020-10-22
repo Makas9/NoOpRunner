4444917c6a6c40e093445894215685c02067d830
gta-6 <lukasziukas18@gmail.com>
2020-10-22 11:06:53 +0300

    Removed unused directives

--- Core/NoOpRunner.Core/PlatformsContainer.cs
+++ Core/NoOpRunner.Core/PlatformsContainer.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using NoOpRunner.Core.Dtos;
 using NoOpRunner.Core.Enums;
 using NoOpRunner.Core.Interfaces;
--- Core/NoOpRunner.Core/PowerUpsContainer.cs
+++ Core/NoOpRunner.Core/PowerUpsContainer.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using NoOpRunner.Core.Dtos;
 using NoOpRunner.Core.Enums;
 using NoOpRunner.Core.Interfaces;
 using NoOpRunner.Core.Shapes;
-using Color = System.Windows.Media.Color;
 
 namespace NoOpRunner.Core
 {
