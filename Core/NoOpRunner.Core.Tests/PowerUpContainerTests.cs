a4ee1981fa1c44c02a370bc5b637ef024bf16743
TautvydasD <tautvydas.diksas@gmail.com>
2020-11-24 15:18:28 +0200

    Added shouldly package

--- Core/NoOpRunner.Core.Tests/PowerUpContainerTests.cs
+++ Core/NoOpRunner.Core.Tests/PowerUpContainerTests.cs
@@ -9,46 +9,47 @@ using NoOpRunner.Core.Enums;
 using NoOpRunner.Core.Interfaces;
 using NoOpRunner.Core.Shapes;
 using Xunit;
+using Shouldly;
 
 namespace NoOpRunner.Core.Tests
 {
     public class PowerUpContainerTests
     {
+        /**
+         * Copy these comments to seperate testing parts
         // Arrange
-
         // Act
-
         // Assert
+         */
 
-        // TODO:
-        // * better implementation
         [Fact] // mostly for stuff tested with same expected variables
-        public void GetPowerUpAt_WithGivenCoordinates_ReturnsPowerUp()
+        public void GetPowerUpAt_WhenValidCoordinatesProvided_ShouldReturnAppropriatePowerup()
         {
-            //arrange
-            //int expectedX = 5;
-            //int expectedY = 5;
+            // Arrange
+            int expected = 123;
 
-            //act
-            //PowerUpsContainer powerUpsContainer;
-            //PowerUp power = powerUpsContainer.GetPowerUpAt(expectedX, expectedY);
-
-            //assert
-            Assert.Equal(1,1);
+            // Act
+            int actual = 123;
+            
+            // Assert
+            // Assert.Equal(1,1); // Old Assert way
+            expected.ShouldBe(actual); // New Assert way
 
         }
 
         [Theory] // testing by passing arguments diffrent sets of data
         [InlineData(4,3,7)]
         [InlineData(5,5,10)]
-        public void passingArguments(double x, double y, double expected)
+        public void PassingArgumentsExample_IfNeededAnArgumentPassExample_ShouldReturnSum(double x, double y, double expected)
         {
             // Arrange
 
             // Act
             double actual = x + y;
+
             // Assert
-            Assert.Equal(expected, actual);
+            //Assert.Equal(expected, actual); // Old Assert way
+            expected.ShouldBe(actual); // New Assert way
         }
     }
 }
