e7b8bfd16bdd4d8940f6a2f3c6c9b80f6e980a98
Rokas Gulbinas <rkgulbinas@gmail.com>
2020-11-25 22:34:17 +0200

    Add shape builder and NoOpRunner tests

---
+++ Core/NoOpRunner.Core.Tests/NoOpRunnerTests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using NoOpRunner.Core.Dtos;
+using NoOpRunner.Core.Interfaces;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace NoOpRunner.Core.Tests
+{
+    public class NoOpRunnerTests
+    {
+        [Fact]
+        public void NoOpRunner_WhenHostingStarted_ShouldCallConnectionManager()
+        {
+            // Arrange
+            var connectionManagerMock = new Mock<IConnectionManager>();
+            var game = new NoOpRunner(connectionManagerMock.Object);
+
+            // Act
+            game.StartHosting();
+
+            //Assert
+            connectionManagerMock.Verify(x => x.Start(It.IsAny<string>(), It.IsAny<Action<MessageDto>>()), Times.Once);
+            game.IsHost.ShouldBe(true);
+        }
+
+        [Fact]
+        public void NoOpRunner_WhenConnecting_ShouldCallConnectionManager()
+        {
+            // Arrange
+            var connectionManagerMock = new Mock<IConnectionManager>();
+            var game = new NoOpRunner(connectionManagerMock.Object);
+
+            // Act
+            game.ConnectToHub().GetAwaiter().GetResult();
+
+            //Assert
+            connectionManagerMock.Verify(x => x.Connect(It.IsAny<string>(), It.IsAny<Action<MessageDto>>()), Times.Once);
+            game.IsHost.ShouldBe(false);
+        }
+
+        [Fact]
+        public void NoOpRunner_OnMapLoopFired_ShouldSendPlatformsUpdate()
+        {
+            // Arrange
+            var connectionManagerMock = new Mock<IConnectionManager>();
+            var game = new NoOpRunner(connectionManagerMock.Object);
+            game.StartHosting();
+
+            // Act
+            game.OnMapMoveLoopFired().GetAwaiter().GetResult();
+
+            //Assert
+            connectionManagerMock.Verify(x => x.SendMessageToClient(It.Is<MessageDto>(m => m.MessageType == Enums.MessageType.PlatformsUpdate)), Times.Once);
+        }
+    }
+}
---
+++ Core/NoOpRunner.Core.Tests/ShapeBuilderTests.cs
@@ -0,0 +1,46 @@
+﻿using NoOpRunner.Core.Builders;
+using NoOpRunner.Core.Configurators;
+using NoOpRunner.Core.Shapes.GenerationStrategies;
+using NoOpRunner.Core.Shapes.StaticShapes;
+using Shouldly;
+using Xunit;
+
+namespace NoOpRunner.Core.Tests
+{
+    public class ShapeBuilderTests
+    {
+        [Fact]
+        public void ShapeBuilder_WhenPassablePlatformConfiguratorIsUsed_ShouldCreatePassablePlatform()
+        {
+            // Arrange
+            var shapeBuilderConfigurator = new ShapeBuilder<PassablePlatformConfigurator>().Configure();
+            shapeBuilderConfigurator.ConfigureBounds(1, 2, 3, 4);
+            shapeBuilderConfigurator.ConfigureGenerationStrategy(new FillGenerationStrategy());
+
+            // Act
+            var result = shapeBuilderConfigurator.Build();
+
+            // Assert
+            result.ShouldBeAssignableTo<PassablePlatform>();
+            result.CenterPosX.ShouldBeInRange(1, 4);
+            result.CenterPosY.ShouldBeInRange(1, 4);
+        }
+
+        [Fact]
+        public void ShapeBuilder_WhenImpassablePlatformConfiguratorIsUsed_ShouldCreateImpassablePlatform()
+        {
+            // Arrange
+            var shapeBuilderConfigurator = new ShapeBuilder<ImpassablePlatformConfigurator>().Configure();
+            shapeBuilderConfigurator.ConfigureBounds(1, 2, 3, 4);
+            shapeBuilderConfigurator.ConfigureGenerationStrategy(new FillGenerationStrategy());
+
+            // Act
+            var result = shapeBuilderConfigurator.Build();
+
+            // Assert
+            result.ShouldBeAssignableTo<ImpassablePlatform>();
+            result.CenterPosX.ShouldBeInRange(1, 4);
+            result.CenterPosY.ShouldBeInRange(1, 4);
+        }
+    }
+}
