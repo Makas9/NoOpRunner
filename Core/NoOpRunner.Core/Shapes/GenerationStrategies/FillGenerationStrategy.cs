391bc9f20afb94214cbe97c9391924ba03276352
Rokas Gulbinas <rkgulbinas@gmail.com>
2020-11-26 11:24:07 +0200

    Revert visibility changes

--- Core/NoOpRunner.Core/Shapes/GenerationStrategies/FillGenerationStrategy.cs
+++ Core/NoOpRunner.Core/Shapes/GenerationStrategies/FillGenerationStrategy.cs
@@ -2,7 +2,7 @@
 
 namespace NoOpRunner.Core.Shapes.GenerationStrategies
 {
-    public class FillGenerationStrategy : GenerationStrategy
+    class FillGenerationStrategy : GenerationStrategy
     {
         public override List<ShapeBlock> GenerateShapeBlocks(int lowerBoundX, int lowerBoundY, int upperBoundX, int upperBoundY, int? startY = null)
         {
